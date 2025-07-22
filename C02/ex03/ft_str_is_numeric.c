3a23d53e20e51796024ef3fec799dc0848a539c4
shevial <sarahevialopez@gmail.com>
2025-07-22 09:10:12 +0200

    enunciados

---
+++ C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,3 @@
+//Escribe una función que devuelva 1 si el string usado como parámetro contiene unicamente digitos y devuelva 0 si contiene otro tipo de caracteres
+//Devolverá 1 si str es un string vacío
+//int ft_str_is_nuemric(char *str)
