4c838dca5b11f568fdb79bb3de55e24c3b2953bb
shevial <sarahevialopez@gmail.com>
2025-07-22 08:50:27 +0200

    enunciados

---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,3 @@
+//Escribe una función que divide los int apuntados por a y b, el resultado de la división se almacena en el int apuntado por a, el resultado del resto de la división se almacena en el int apuntado por b
+//void ft_ultimate_div_mod(int *a, int *b)
+
---
+++ C01/ex06/ft_strlen.c
@@ -0,0 +1,2 @@
+// Escribe una función que cuente el número de caracteres de un string que devuelva el número encontrado
+// int ft_strlen(char *str)
