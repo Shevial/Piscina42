f8628f801cc019e679393dd85684a39e58669dde
shevial <sarahevialopez@gmail.com>
2025-07-22 08:47:02 +0200

    enunciados

---
+++ C01/ex05/ft_putstr.c
@@ -0,0 +1,4 @@
+// Escribe una función que muestre uno a uno en la pantalla los caracteres de un string
+// La dirección del primer carácter del string está incluida en el puntero utilizado como parámetro para la función
+// void ft_putstr(char *str)
+
